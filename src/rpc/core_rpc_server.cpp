6e18f99b4ce8fb851351f66b600f7bedce5e88a9
Arnis S <arnis.salms@gmail.com>
2021-12-23 15:32:53 +0200

    Change software name(as well as dw link) for rpc server

--- src/rpc/core_rpc_server.cpp
+++ src/rpc/core_rpc_server.cpp
@@ -1570,7 +1570,7 @@ namespace cryptonote
   //------------------------------------------------------------------------------------------------------------------------------
   bool core_rpc_server::on_update(const COMMAND_RPC_UPDATE::request& req, COMMAND_RPC_UPDATE::response& res)
   {
-    static const char software[] = "monero";
+    static const char software[] = "lethean";
 #ifdef BUILD_TAG
     static const char buildtag[] = BOOST_PP_STRINGIZE(BUILD_TAG);
     static const char subdir[] = "cli";
